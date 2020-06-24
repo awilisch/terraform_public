1b78aaf47c05c9dc1fbc66589b2ce08c5875fba2
Aric Wilisch <awilisch@net-collective.net>
2020-06-23 21:07:37 -0700

    Modulerize docker local blog

--- local/docker/blog/variables.tf
+++ local/docker/blog/variables.tf
@@ -1,3 +1,11 @@
+variable "container_name" {
+  description = "Name of container"
+  default = "blog"
+}
+variable "image" {
+  description = "image for container"
+  default = "ghost:latest"
+}
 variable "int_port" {
   description = "internal port for container"
   default = "2368"
