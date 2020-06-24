d16b5345b0e1e52308359ad7dfa085ab78c08934
Aric Wilisch <awilisch@net-collective.net>
2020-06-23 21:13:53 -0700

    Modulerize docker local blog

--- local/docker/blog/modules/container/main.tf
+++ local/docker/blog/modules/container/main.tf
@@ -1,7 +1,7 @@
 # Start the container
 resource "docker_container" "container_id" {
-    name = "var.name
-    image = "var.image
+    name = var.name
+    image = var.image
     ports {
         internal = var.int_port
         external = var.ext_port
